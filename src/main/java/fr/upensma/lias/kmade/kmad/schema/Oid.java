7d7a1591b66d58d16bc36761e60b4516947a32e2
baronm <baronm@92cdedf5-4e3b-5e40-8ebf-1f118bb6e087>
2011-09-06 08:11:24 +0000

    git-svn-id: http://193.55.163.233/svn/KMADe/trunk@2434 92cdedf5-4e3b-5e40-8ebf-1f118bb6e087

--- src/main/java/fr/upensma/lias/kmade/kmad/schema/Oid.java
+++ src/main/java/fr/upensma/lias/kmade/kmad/schema/Oid.java
@@ -38,7 +38,7 @@ public class Oid implements Comparable<Object>, Serializable {
 
     public Oid(String st) {
     	//Previous and actual version of the dtd
-	if (st.charAt(0) != '#' || st.charAt(0) != 'K') {
+	if (st.charAt(0) != '#' && st.charAt(0) != 'K') {
 	    System.err.println("Erreur dans chaine pour oid st=" + st);
 	}
 	Integer in = Integer.valueOf(Integer.parseInt(st.substring(1)));
