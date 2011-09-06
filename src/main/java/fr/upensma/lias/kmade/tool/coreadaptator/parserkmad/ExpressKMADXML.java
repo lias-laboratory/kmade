66113e8a14c3722532b44fcd4058a7d0f010a0f9
baronm <baronm@92cdedf5-4e3b-5e40-8ebf-1f118bb6e087>
2011-09-06 08:21:39 +0000

    git-svn-id: http://193.55.163.233/svn/KMADe/trunk@2445 92cdedf5-4e3b-5e40-8ebf-1f118bb6e087

--- src/main/java/fr/upensma/lias/kmade/tool/coreadaptator/parserkmad/ExpressKMADXML.java
+++ src/main/java/fr/upensma/lias/kmade/tool/coreadaptator/parserkmad/ExpressKMADXML.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -37,7 +36,6 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
