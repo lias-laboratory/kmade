b76093a61df72d20d7f6da794511628f51a58155
lachaumet <lachaumet@92cdedf5-4e3b-5e40-8ebf-1f118bb6e087>
2012-10-15 14:42:38 +0000

    git-svn-id: http://193.55.163.233/svn/KMADe/trunk@3553 92cdedf5-4e3b-5e40-8ebf-1f118bb6e087

--- src/main/java/fr/upensma/lias/kmade/tool/coreadaptator/prototype/ExpressionTree.java
+++ src/main/java/fr/upensma/lias/kmade/tool/coreadaptator/prototype/ExpressionTree.java
@@ -15,126 +15,119 @@
 * You should have received a copy of the GNU Lesser General Public License
 * along with KMADe.  If not, see <http://www.gnu.org/licenses/>.
 **********************************************************************************/
+
 package fr.upensma.lias.kmade.tool.coreadaptator.prototype;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- * @author Thomas Lachaume
- */
 public class ExpressionTree {
 
-    public String value;
-    
-    public ExpressionTree left;
-    
-    public ExpressionTree right;
+	public String value;
+	public ExpressionTree left;
+	public ExpressionTree right;
 
-    public ExpressionTree(String value, ExpressionTree left,
-	    ExpressionTree right) {
-	this.value = value;
-	this.left = left;
-	this.right = right;
-    }
 
-    public ChoiceEnum evaluate(HashMap<String, ChoiceEnum> map) {
-
-	ChoiceEnum res = eval_rec(map);
-	System.err.println("mon res c  : " + res);
-	return res;
-    }
-
-    @Override
-    public String toString() {
-	String res = value;
-	if (left != null) {
-	    res += left.value;
-	}
-	if (right != null) {
-	    res += right.value;
-	}
-	res += "\n";
-	if (left != null) {
-	    res += left.toString();
-	}
-	if (right != null) {
-	    res += right.toString();
+	public ExpressionTree(String value ,ExpressionTree left,ExpressionTree right) {
+		this.value = value;
+		this.left = left;
+		this.right = right;
 	}
 
-	return res;
-    }
 
-    private ChoiceEnum eval_rec(HashMap<String, ChoiceEnum> map) {
-	System.err.println("in 2 :" + this);
-	if (value == null) {
-	    return null;
-	} else {
-	    if (value.equals("!")) {
-		ChoiceEnum l = left.eval_rec(map);
-		switch (l) {
-		case faux:
-		    return ChoiceEnum.vrai;
-		case indeterminée:
-		    return ChoiceEnum.indeterminée;
-		case vrai:
-		    return ChoiceEnum.faux;
+	public ChoiceEnum evaluate(HashMap<String, ChoiceEnum> map) {
+
+		ChoiceEnum res = eval_rec(map);
+		return res;
+	}
+
+	@Override
+	public String toString() {
+		String res =  value   ;
+		if(left!=null){
+			res += left.value;
 		}
-	    } else if (value.equals("*")) {
-		ChoiceEnum l = left.eval_rec(map);
-		ChoiceEnum r = right.eval_rec(map);
-		if (l == ChoiceEnum.vrai) {
-		    return r;
-		} else if (l == ChoiceEnum.faux) {
-		    return l;
-		} else if (l == ChoiceEnum.indeterminée) {
-		    if (r == ChoiceEnum.faux) {
-			return r;
-		    } else {
-			return l;
-		    }
+		if(right!=null){
+			res += right.value;
 		}
-	    } else if (value.equals("+")) {
-		ChoiceEnum l = left.eval_rec(map);
-		ChoiceEnum r = right.eval_rec(map);
-		if (l == ChoiceEnum.vrai || r == ChoiceEnum.vrai) {
-		    return ChoiceEnum.vrai;
-		} else if (l == ChoiceEnum.indeterminée
-			|| r == ChoiceEnum.indeterminée) {
-		    return ChoiceEnum.indeterminée;
-		} else {
-		    return ChoiceEnum.faux;
+		res +="\n";
+		if(left!=null){
+			res += left.toString();
 		}
-	    } else if (value != null) {
-		if (map.get(value) != null) {
-		    System.out
-			    .println("map valueeeeeeeeeeeee" + map.get(value));
-		    return map.get(value);
+		if(right!=null){
+			res += right.toString();
 		}
-		System.out.println("pas de map value!!");
-	    } else {
-		System.err.println("hahaah");
-	    }
+
+		return res;
+	}
+
+
+	private ChoiceEnum eval_rec(HashMap<String,ChoiceEnum> map){
+		System.err.println("in 2 :" +this);
+		if(value == null){
+			return null;
+		}else {
+			if(value.equals("!")){
+				ChoiceEnum l = left.eval_rec(map);
+				switch(l){
+				case faux : return ChoiceEnum.vrai;
+				case indeterminée : return ChoiceEnum.indeterminée ;
+				case vrai : return ChoiceEnum.faux;
+				}
+			} else if (value.equals("*")){
+				ChoiceEnum l = left.eval_rec(map);
+				ChoiceEnum r = right.eval_rec(map);
+				if(l == ChoiceEnum.vrai){
+					return r; 
+				}else if (l == ChoiceEnum.faux){
+					return l;
+				} else if (l == ChoiceEnum.indeterminée){
+					if(r == ChoiceEnum.faux){
+						return r;
+					}else{
+						return l;
+					}			
+				}
+			} else if (value.equals("+")){
+				ChoiceEnum l = left.eval_rec(map);
+				ChoiceEnum r = right.eval_rec(map);
+				if(l == ChoiceEnum.vrai || r == ChoiceEnum.vrai){
+					return ChoiceEnum.vrai;
+				}else if(l == ChoiceEnum.indeterminée || r == ChoiceEnum.indeterminée){
+					return ChoiceEnum.indeterminée;
+				}else{
+					return ChoiceEnum.faux;
+				}
+			}else if (value != null){
+				if( map.get(value) != null ){
+					System.out.println("map valueeeeeeeeeeeee"+map.get(value));
+					return map.get(value);
+				}
+				System.out.println("pas de map value!!");
+			}else {
+				System.err.println("hahaah");
+			}
+
+		}
+		return null;
 
 	}
-	return null;
 
-    }
 
-    public String getDescription() {
-	String res = "";
-	if (value == null) {
-
-	} else if (value.equals("+")) {
-	    res += "(" + left.getDescription() + " OU "
-		    + right.getDescription() + ")";
-	} else if (value.equals("*")) {
-	    res += "(" + left.getDescription() + " ET "
-		    + right.getDescription() + ")";
-	} else if (value.equals("!")) {
-	    res += "(NON " + left.getDescription() + ")";
-	} else {
-	    res += value;
+	public String getDescription() {
+		String res ="";
+		if(value == null){
+			
+		}else if(value.equals("+")){
+			res += "(" + left.getDescription() + " OU " + right.getDescription() +")";
+		}else if(value.equals("*")){
+			res += "(" + left.getDescription() + " ET " + right.getDescription() +")";
+		}else if(value.equals("!")){
+			res += "(NON " + left.getDescription()+")";
+		}else{
+			res+=value;
+		}
+		return res;
 	}
-	return res;
-    }
+
 }
